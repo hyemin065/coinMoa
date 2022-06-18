577fab51a30f2c52eac356f4ab42677a7ac90df1
hyemin065 <p62517@gmail.com>
2022-06-18 23:59:18 +0900

    feat: loading

--- src/pages/PortFolio/PortFolio.tsx
+++ src/pages/PortFolio/PortFolio.tsx
@@ -11,6 +11,8 @@ import PortFolioAssets from './PortFolioAssets/PortFolioAssets';
 
 import styles from './portFolio.module.scss';
 
+import Loading from '../../assets/loading.gif';
+
 const PortFolio = () => {
   const [userCoinList, setUserCoinList] = useRecoilState<IUserCoinList[]>(coinListState);
   const setBookMarkCoinName = useSetRecoilState(bookMarkCoinNameState);
@@ -19,6 +21,7 @@ const PortFolio = () => {
   const [marketList, setMarketCoinList] = useState<IUserCoinList[]>([]);
   const [isOpenModal, setIsOpenModal] = useRecoilState(modalState);
   const [marketValue, setMarketValue] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const list = marketValue ? marketList : userCoinList;
 
@@ -32,7 +35,10 @@ const PortFolio = () => {
 
   const getPortFolio = async () => {
     const data = await getPortFolioApi();
-    setUserCoinList(data);
+    if (data.length > 0) {
+      setUserCoinList(data);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -42,60 +48,68 @@ const PortFolio = () => {
 
   return (
     <div className={styles.container}>
-      {isLogin ? (
+      {!isLoading ? (
         <>
           <PortFolioAssets setMarketValue={setMarketValue} setMarketCoinList={setMarketCoinList} />
           <div className={styles.portFolioContents}>
-            {list.length > 0 ? (
-              <>
-                <div className={styles.addBtn}>
-                  <button
-                    type='button'
-                    onClick={() => {
-                      handleOpenModal();
-                      handleBookMarkGetName('');
-                    }}
-                  >
-                    추가하기
-                  </button>
-                </div>
-
-                <table className={styles.portFolioTable}>
-                  <thead>
-                    <tr>
-                      <th>거래일</th>
-                      <th>거래소</th>
-                      <th colSpan={2}>코인</th>
-                      <th>현재가</th>
-                      <th>평단</th>
-                      <th>수량</th>
-                      <th>매수금액</th>
-                      <th>평가금액</th>
-                      <th>평가손익</th>
-                      <th>수익률</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {list.map((item: IUserCoinList) => {
-                      return <PortFolioItem key={`${item.market}_${item.symbol}`} item={item} />;
-                    })}
-                  </tbody>
-                </table>
-              </>
+            {isLogin ? (
+              <div>
+                {list.length > 0 ? (
+                  <>
+                    <div className={styles.addBtn}>
+                      <button
+                        type='button'
+                        onClick={() => {
+                          handleOpenModal();
+                          handleBookMarkGetName('');
+                        }}
+                      >
+                        추가하기
+                      </button>
+                    </div>
+                    <table className={styles.portFolioTable}>
+                      <thead>
+                        <tr>
+                          <th>거래일</th>
+                          <th>거래소</th>
+                          <th colSpan={2}>코인</th>
+                          <th>현재가</th>
+                          <th>평단</th>
+                          <th>수량</th>
+                          <th>매수금액</th>
+                          <th>평가금액</th>
+                          <th>평가손익</th>
+                          <th>수익률</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {list.map((item: IUserCoinList) => {
+                          return <PortFolioItem key={`${item.market}_${item.symbol}`} item={item} />;
+                        })}
+                      </tbody>
+                    </table>
+                  </>
+                ) : (
+                  <div className={styles.addBtnWrap}>
+                    <button type='button' onClick={handleOpenModal}>
+                      추가하기
+                    </button>
+                  </div>
+                )}
+              </div>
             ) : (
-              <div className={styles.addBtnWrap}>
-                <button type='button' onClick={handleOpenModal}>
-                  추가하기
-                </button>
+              <div className={styles.goLogin}>
+                <p>로그인 후 이용하실 수 있습니다</p>
+                <Link to='/signin'>로그인</Link>
               </div>
             )}
+
             {isOpenModal && <Modal />}
           </div>
         </>
       ) : (
-        <div className={styles.goLogin}>
-          <p>로그인 후 이용하실 수 있습니다</p>
-          <Link to='/signin'>로그인</Link>
+        <div className={styles.loadingWrap}>
+          <img src={Loading} alt='loading' />
         </div>
       )}
     </div>
